63ff8df9b24be0f0ca6a9c4fc7d7378a1437fa73
JesusD03 <1006258541@u.icesi.edu.co>
2024-04-22 15:02:05 -0500

    ya funciona el login, falla al pasar al perfil

--- app/src/main/java/icesi/edu/co/piecefood/viewmodel/LoginViewModel.kt
+++ app/src/main/java/icesi/edu/co/piecefood/viewmodel/LoginViewModel.kt
@@ -6,9 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import icesi.edu.co.piecefood.model.AppAuthState
 import icesi.edu.co.piecefood.repository.AuthRepository
+import icesi.edu.co.piecefood.repository.AuthRepositoryImpl
 import kotlinx.coroutines.launch
 
 class LoginViewModel(private val authRepository: AuthRepository) : ViewModel() {
+
+    // Constructor sin argumentos para ViewModelProvider
+    constructor() : this(AuthRepositoryImpl()) // Proporciona una instancia predeterminada de AuthRepository
 
     private val _authState = MutableLiveData<AppAuthState>()
     val authState: LiveData<AppAuthState>
@@ -22,3 +26,4 @@ class LoginViewModel(private val authRepository: AuthRepository) : ViewModel() {
         }
     }
 }
+
