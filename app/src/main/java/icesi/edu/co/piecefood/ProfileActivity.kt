39421198a2dc2ad154908790e1df5b93d34419a8
Juan Diego <juandiroca@hotmail.com>
2024-06-03 18:24:38 -0500

    Se corrigio error en profile.xlm

--- app/src/main/java/icesi/edu/co/piecefood/ProfileActivity.kt
+++ app/src/main/java/icesi/edu/co/piecefood/ProfileActivity.kt
@@ -16,13 +16,12 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding.root)
+
 
         viewmodel.loadUser() //viewmodel.observeUser()
 
         viewmodel.userState.observe(this){
             binding.emailTV.text = it.email
-            binding.usernameTV.text = it.username
         }
     }
 }
